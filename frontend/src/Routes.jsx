5a554037e2bdf46ac7fb5f4f0f4ee92d7971e04d
Akashkolhe56 <akashkolhe089@gmail.com>
2025-01-28 13:15:30 +0530

    Milestone 9 completed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,6 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
-export { LoginPage, SignupPage, Home };
+
+import CreateProduct from "./pages/createProject";
+export { LoginPage, SignupPage, Home,CreateProduct };
